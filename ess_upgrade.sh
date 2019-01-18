f156d008c0f77f617ddfd48299a7bb6e38bfe0a8
AsherHsieh <asherhsiang@wisers.com>
2019-01-18 16:30:51 +0800

    imporve some tasks

--- ess_upgrade.sh
+++ ess_upgrade.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# input the started date
-ansible-playbook "inventories/inventory_$@/playbook.yml" \
-                 --inventory="inventories/inventory_$@/hosts"
+if [ -d "inventories/inventory_date/" ]; then
+    # input the started date as folder name
+    mv inventories/inventory_date/ inventories/inventory_$@/
+    sed -i s/inventory_date/inventory_$@/g ansible.cfg
+else
+    echo "[Notice] Please use started date as variable"
+fi
 
+ansible-playbook --inventory="inventories/inventory_$@/hosts" -b -c ssh --ask-pass inventories/inventory_$@/playbook.yml
